d9e0dceb8e9e4c83ea86ab23396ef6219773a1b5
Jonathan Jaegerman <jonathan.jaegerman@gmail.com>
2023-02-09 21:32:16 -0500

    Switched from Material UI to Amplify UI

--- src/index.js
+++ src/index.js
@@ -3,23 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import store from './store/store'
-import { Provider } from 'react-redux'
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import store from './store/store';
+import { Provider } from 'react-redux';
+import '@aws-amplify/ui-react/styles.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link
+      href="https://fonts.googleapis.com/css2?family=Inter:slnt,wght@-10..0,100..900&display=swap"
       rel="stylesheet"
-      href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-    />
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/icon?family=Material+Icons"
     />
     <App />
   </Provider>
